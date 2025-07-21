96ef3ae88ace739f865e0f646259366b6c206430
Igor Tiburcio Cavalcanti <igor.tiburcio.cs@gmail.com>
2025-07-20 21:18:19 -0300

    chore: add ESLint and Prettier with base configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default [
+    { files: ['**/*.{js,mjs,cjs,ts}'] },
+    { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+    { languageOptions: { globals: globals.node } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintConfigPrettier,
+    {
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/interface-name-prefix': 'off',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            'no-unused-vars': 'off',
+            '@typescript-eslint/no-unused-vars': [
+                'error',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    caughtErrorsIgnorePattern: '^_',
+                },
+            ],
+            'no-case-declarations': 'off',
+            'no-useless-escape': 'off',
+        },
+    },
+]
